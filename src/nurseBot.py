d7b1564cd88da74ff840f6ec1b17c6474264d6c1
Zachary Goodsell <zl22good@siena.edu>
2019-11-25 11:03:36 -0500

    changed voice

--- src/nurseBot.py
+++ src/nurseBot.py
@@ -43,7 +43,7 @@ def main():
 	wantFood = "null"
 	wantNurse = "null"
 	foundMarker = "null"
-	voice = 'voice_ked_diphone'
+	voice = 'voice_rab_diphone'
 	
 	#Wait for nurse to tell which room to see
 	validAnswer = False
